3059c8ac4ed8b4757f6a3d8d3e5cfce46c5644d3
Kyle Cuthbertson <KyleKCuthbertson@gmail.com>
2021-11-07 16:54:17 +0000

    Implemented Basic UI for Profile Menu

--- src/components/buttons/Profile.js
+++ src/components/buttons/Profile.js
@@ -1,9 +1,23 @@
 
 
-const Profile = () => {
+const Profile = (props) => {
+
+  const { menu, toggleMenu } = props;
+
   return (
     <>
-      <button className="profile-btn"><i className="fas fa-user-circle"></i></button>
+      <button onClick={() => toggleMenu(true)} className="profile-btn"><i className="fas fa-user-circle"></i></button>
+      <div className={menu ? "profile-menu show" : "profile-menu"}>
+        <span onClick={() => toggleMenu(false)} className="btn-close"><i class="fas fa-times"></i></span>
+        <div className="profile-menu-wrapper">
+          <ul className="profile-menu-list">
+            <li>Home</li>
+            <li>My Profile</li>
+            <li>Settings</li>
+            <li>Log Out</li>
+          </ul>
+        </div>
+      </div>
     </>
   )
 }
--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 
 import Profile from '../../components/buttons/Profile';
 
 const Header = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const openMenuToggle = (menuToggle) => {
+    setMenuOpen(menuToggle);
+    console.log(menuToggle);
+  }
+
   return (
     <>
       <header>
         <h2 className="app-title">ConnectMe</h2>
-        <Profile/>
+        <Profile menu={menuOpen} toggleMenu={openMenuToggle} />
       </header>
     </>
   )
